80f221d386fdf05645a902fc52d0ca244d530f76
Mikołaj Mroczkowski <mikolaj.mroczkowski@gmail.com>
2021-11-23 16:35:14 +0100

    Znaki specjalne i cyfry

--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 def decode(txt,key):
     otp = ""
     for obj in txt:
-        isupper = obj.isupper()
-        obj = obj.lower()
-        kod = ord(obj)
-        kod -= key
-        if kod < 97:
-            kod += 26
-        if isupper:
-            otp += chr(kod).upper()
+        if obj.isalpha():
+            isupper = obj.isupper()
+            obj = obj.lower()
+            kod = ord(obj)
+            kod -= key
+            if kod < 97:
+                kod += 26
+            if isupper:
+                otp += chr(kod).upper()
+            else:
+                otp += chr(kod)
         else:
-            otp += chr(kod)
+            otp += obj
     return otp
 
 def encode(txt,key):
     otp = ""
     for obj in txt:
-        isupper = obj.isupper()
-        obj = obj.lower()
-        kod = ord(obj)
-        kod += key
-        if kod > 112:
-            kod -= 26
-        if isupper:
-            otp += chr(kod).upper()
+        if obj.isalpha():
+            isupper = obj.isupper()
+            obj = obj.lower()
+            kod = ord(obj)
+            kod += key
+            if kod > 112:
+                kod -= 26
+            if isupper:
+                otp += chr(kod).upper()
+            else:
+                otp += chr(kod)
         else:
-            otp += chr(kod)
+            otp += obj
     return otp
-str="AZ"
+str="ZAZA!1"
 print(encode(str,3),decode(encode(str,3),3),sep=";")
