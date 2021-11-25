c112ab43b0d50edecd66b483f303fd5728b4600c
Mikołaj Mroczkowski <mikolaj.mroczkowski@gmail.com>
2021-11-25 08:50:17 +0100

    Proste tekstowe ui w funkcji main()

--- main.py
+++ main.py
@@ -33,5 +33,33 @@ def encode(txt,key):
         else:
             otp += obj
     return otp
-str="ZAZA!1"
-print(encode(str,3),decode(encode(str,3),3),sep=";")
+
+def main():
+    while True:
+        print("Wybierz tryb:","Dla Szyfrowania wpisz E","Dla deszyfrowania wpisz D",sep="\n")
+        mode = input()
+        if mode == "E":
+            print("Moduł Szyfrowania\nAby Opuścić wpisz frazę EXIT")
+            while True:
+                print("Podaj text")
+                txt = input()
+                if txt=="EXIT":
+                    break;
+                print("Podaj klucz")
+                key=input()
+                print("Szyfr "+txt+" z kluczem "+key+" to:")
+                print(encode(txt,int(key)))
+        elif mode == "D":
+            print("Moduł Deszyfrowania")
+            while True:
+                print("Podaj text")
+                txt = input()
+                if txt=="EXIT":
+                    break;
+                print("Podaj klucz")
+                key=input()
+                print("Deszyfr "+txt+" z kluczem "+key+" to:")
+                print(decode(txt,int(key)))
+        else:
+            print("nierozponano modułu")
+main()
